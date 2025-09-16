26028a62531a31c25cbd4948eeae890cd3899ecc
Lanna <lannadosanjoscosta@gmail.com>
2025-09-16 00:09:55 -0300

    Delete flask_mvc directory

--- flask_mvc/models/personagem.py
+++
@@ -1,7 +0,0 @@
-class Personagem:
-    def __init__(self, nome, atributos, raca, classe):
-        self.nome = nome
-        self.atributos = atributos
-        self.raca = raca
-        self.classe = classe   
-        
